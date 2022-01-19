2a591b2e2aa22e77454a385e4ceee3cc54299643
이기준 <2stndard@gmail.com>
2022-01-19 18:00:11 +0900

    220119-1800

--- chap5/chap5.R
+++ chap5/chap5.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(patchwork)
+library(showtext)
+showtext_auto()
 
 df_입학자_long_total <- df_입학자_long |>
   filter(지역 == '전체')
@@ -73,5 +75,71 @@ stat_summary()
 p_col2 + 
   geom_col(position = 'dodge')
 
+library(readxl)
+df_nation <- read_xlsx('d:/R/git/datavisualization/chap5/연도별 유학국가별 유학생수.xlsx', sheet = 'Sheet0', skip = 2, col_types = c('numeric', 'text', rep('numeric', 25)), col_names = TRUE)
+
+df_nation <- df_nation |>
+  filter(!is.na(학년도), 학제 == '소계') |>
+  select(!contains(c('계', '학제', '기타', '미확인', '그외동남아', '남미'))) |>
+  gather('국가명', '유학생수', -'학년도')
+
+df_nation_top10 <- df_nation |>
+  group_by(국가명) |>
+  summarise(sum = sum(유학생수)) |>
+  arrange(desc(sum)) |>
+  top_n(10)
+
+df_nation_top10
 
 
+df_nation_top10 |>
+  ggplot(aes(x = 국가명, y = sum)) +
+  geom_col(fill = 'dark blue') +
+  geom_text(aes(x = 국가명, y = sum, label = sum), vjust = -0.5) + 
+  theme_minimal() + 
+  labs(title = '국가별 유학생수 Top 10', y = '유학생수')
+
+
+df_nation_top10$국가명 <- fct_reorder(df_nation_top10$국가명, desc(df_nation_top10$sum))
+
+p_nation_top10 <- df_nation_top10 |>
+  ggplot(aes(x = 국가명, y = sum)) +
+  geom_col(fill = 'dark blue') +
+  geom_text(aes(x = 국가명, y = sum, label = sum), vjust = -0.5) + 
+  theme_minimal() + 
+  labs(title = '국가별 유학생수 Top 10', y = '유학생수')
+
+
+flag_usa <- 'd:/R/git/datavisualization/chap5/usa.png'
+flag_canada <- 'd:/R/git/datavisualization/chap5/can.png'
+flag_china <- 'd:/R/git/datavisualization/chap5/chi.png'
+flag_phi <- 'd:/R/git/datavisualization/chap5/phi.png'
+flag_nz <- 'd:/R/git/datavisualization/chap5/nz.png'
+flag_aus <- 'd:/R/git/datavisualization/chap5/aus.png'
+flag_jap <- 'd:/R/git/datavisualization/chap5/jap.png'
+flag_eng <- 'd:/R/git/datavisualization/chap5/eng.png'
+flag_mal <- 'd:/R/git/datavisualization/chap5/mal.png'
+flag_ger <- 'd:/R/git/datavisualization/chap5/ger.png'
+
+flags <- data.frame(nations = c('미국', '캐나다', '중국','필리핀', '뉴질랜드', '호주', '일본', '영국', '말레이시아', '독일'), flag_path = c(flag_usa, flag_canada, flag_china, flag_phi, flag_nz, flag_aus, flag_jap, flag_eng, flag_mal, flag_ger))
+
+labels <- setNames(
+  paste0("<img src='", flags$flag_path, "' width='30'  height = '20'> <br> ", flags$nations),  flags$nations)
+
+
+p_nation_top10_1 <- p_nation_top10 +
+  scale_x_discrete(labels = labels)
+
+p_nation_top10_1
+
+
+if(!require(ggtext)) {
+  install.packages('ggtext')
+  library(ggtext)
+}
+library(ggtext)
+
+p_nation_top10_1 +
+  theme(axis.text.x = ggtext::element_markdown())
+
+install.packages('Rcpp')
