4d7fdfa205464b99f0dd8b1367f4c5a6707d1c3c
이기준 <2stndard@gmail.com>
2022-04-12 17:57:41 +0900

    220412-1757

--- plotly/dash.R
+++ plotly/dash.R
@@ -22,6 +22,38 @@ fig <- df_sunburst_cases |>
   )
 
 
+fig <- plot_ly(
+  type = 'scatterpolar',
+  fill = 'tonext'
+) 
+fig <- fig %>%
+  add_trace(
+    r = c(45, 45, 10, 45, 45, 45),
+    theta = c('A','B','C', 'D', 'E', 'A'),
+    name = 'Group A'
+  ) 
+fig <- fig %>%
+  add_trace(
+    r = c(1.5, 10, 39, 31, 15, 1.5),
+    theta = c('A','B','C', 'D', 'E', 'A'),
+    name = 'Group B'
+  ) 
+fig <- fig %>%
+  layout(
+    polar = list(
+      radialaxis = list(
+        visible = T,
+        range = c(0,50)
+      )
+    )
+  )
+
+fig
+
+
+
+
+
 
 library(dash)
 library(dashCoreComponents)
